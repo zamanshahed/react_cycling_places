2d9af2e45f954ac07c056da7eab07936dd2b1a50
zamanshahed <shaheduz.zaman@northsouth.edu>
2020-04-04 22:35:23 +0600

    initial commit 0.55: the navbar frontend development for mobile screens is on its way almost

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -3,10 +3,18 @@ import { Link } from 'react-router-dom';
 
 import MainHeader from './MainHeader';
 import NavLinks from './NavLinks';
+import SideDrawer from './SideDrawer';
 import './MainNavigation.css';
 
 const MainNavigation = props => {
-	return (<MainHeader>
+	return (
+		<React.Fragment>
+	<MainHeader>
+		<SideDrawer>
+			<nav className="main-navigation__drawer-nav">
+				<NavLinks />
+			</nav>
+		</SideDrawer>
 		<button className="main-navigation__menu-btn">
 			<span />
 			<span />
@@ -15,11 +23,13 @@ const MainNavigation = props => {
 		<h1 className="main-navigation__title">
 			<Link to="/">Cycling.Diary</Link>
 		</h1>
-		<nav>
+		<nav className="main-navigation__header-nav">
 			<NavLinks />
 		</nav>
 
-	</MainHeader>);
+	</MainHeader>
+	</React.Fragment>
+	);
 };
 
 export default MainNavigation;
