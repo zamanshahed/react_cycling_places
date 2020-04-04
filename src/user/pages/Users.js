93e4a4cf1ca265ea739776ba61a84a4a051396a5
zamanshahed <shaheduz.zaman@northsouth.edu>
2020-04-04 13:56:39 +0600

    initial commit 0.35: User dummy data is now much more polished with added react-router-dom link property with it to work dynamically

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -8,7 +8,7 @@ const Users = () => {
 	{
 		id: 'u1', 
 		name: 'John Doe', 
-		image:'https://images.pexels.com/photos/171945/pexels-photo-171945.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 
+		image:'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 
 		places: 4
 	}
 	];
