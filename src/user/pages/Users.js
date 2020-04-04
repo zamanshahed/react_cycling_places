070b2097510bc2aed4aa3fbf49d0aa0a79f6940b
zamanshahed <shaheduz.zaman@northsouth.edu>
2020-04-04 09:19:21 +0600

    initial commit 0.3: user frontend on progress. tested with dummy USER array and info showed properly

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
+import UsersList from '../components/UsersList';
+
 const Users = () => {
-	return <h2>
-		Users Online and functioning !
-	</h2>
+
+	const USERS = [
+	{
+		id: 'u1', 
+		name: 'John Doe', 
+		image:'https://images.pexels.com/photos/171945/pexels-photo-171945.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 
+		places: 4
+	}
+	];
+
+	return <UsersList items={USERS}/>;
+		
 };
 
 export default Users;
