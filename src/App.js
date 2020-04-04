3365cb53d1ad3a0fbfc28973c3d1449d4507db59
zamanshahed <shaheduz.zaman@northsouth.edu>
2020-04-04 19:46:39 +0600

    initial Commit 0.5: Navbar and Navlinks are working for the desktop view of the webApp!

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-d
 
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 const App = () => {
   return (
   	<Router>
+      <MainNavigation />
+      <main>
   		<Switch>
   		<Route path="/" exact>
   			<Users/>
@@ -16,6 +19,7 @@ const App = () => {
 	  	</Route>
 	  	<Redirect to="/"/>
 	  	</Switch>
+      </main>
     </Router>
   );
 }
