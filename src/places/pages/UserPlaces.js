e0e0f4db8a92f566eb074dcbbd2b757de1225cc7
zamanshahed <shaheduz.zaman@northsouth.edu>
2020-04-05 21:00:01 +0600

    initial commit 0.65: places are working with a dummy place list array.

---
+++ src/places/pages/UserPlaces.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import PlaceList from '../components/PlaceList';
+
+const DUMMY_PLACES = [
+
+	{
+		id: 'p1',
+		title: 'BMA Road',
+		description: 'One of the most beautiful roads in Ctg for cycling',
+		imageUrl: 'https://media-cdn.tripadvisor.com/media/photo-o/18/93/a8/1b/vatiary-lake.jpg',
+		address: 'Bhatiari - Hathazari Link Rd, Bhatiari',
+		location: {
+			lat: 22.6302981,
+			lng: 91.4596408
+		},
+		creator: 'u1'
+	},
+
+	{
+		id: 'p2',
+		title: 'BMA Road',
+		description: 'One of the most beautiful roads in Ctg for cycling',
+		imageUrl: 'https://media-cdn.tripadvisor.com/media/photo-o/18/93/a8/1b/vatiary-lake.jpg',
+		address: 'Bhatiari - Hathazari Link Rd, Bhatiari',
+		location: {
+			lat: 22.6302981,
+			lng: 91.4596408
+		},
+		creator: 'u2'
+	}
+
+];
+
+const UserPlaces = () => {
+
+
+	return (
+		<PlaceList items={DUMMY_PLACES} />
+	);
+};
+
+export default UserPlaces;
--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -35,7 +35,7 @@ const MainNavigation = props => {
           <span />
         </button>
         <h1 className="main-navigation__title">
-          <Link to="/">YourPlaces</Link>
+          <Link to="/">CyclingPlaces</Link>
         </h1>
         <nav className="main-navigation__header-nav">
           <NavLinks />
