8a25549c77f71c436a2315e2ac14119d211ad406
zamanshahed <shaheduz.zaman@northsouth.edu>
2020-04-06 21:28:41 +0600

    filters added for places to show only of that one userId

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 import PlaceList from '../components/PlaceList';
 
@@ -33,10 +34,11 @@ const DUMMY_PLACES = [
 ];
 
 const UserPlaces = () => {
-
+	const userId =  useParams().userId;
+	const loadedPlaces = DUMMY_PLACES.filter(place=>place.creator === userId);
 
 	return (
-		<PlaceList items={DUMMY_PLACES} />
+		<PlaceList items={loadedPlaces} />
 	);
 };
 
