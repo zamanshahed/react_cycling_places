f728a3f9ace247b51aca5ab8a90449901919a307
zamanshahed <shaheduz.zaman@northsouth.edu>
2020-04-07 21:02:31 +0600

    initial commit 0.82: files rearranged! new place add option with description and logical button added!

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -6,14 +6,14 @@ import PlaceList from '../components/PlaceList';
 const DUMMY_PLACES = [
   {
     id: 'p1',
-    title: 'BMA Road',
-    description: 'One of the most beautiful roads in Ctg for cycling!',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world!',
     imageUrl:
-      'https://media-cdn.tripadvisor.com/media/photo-o/18/93/a8/1b/vatiary-lake.jpg',
-    address: 'Bhatiari - Hathazari Link Rd, Bhatiari',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+    address: '20 W 34th St, New York, NY 10001',
     location: {
-      lat: 22.6302981,
-      lng: 91.4596408 
+      lat: 40.7484405,
+      lng: -73.9878584
     },
     creator: 'u1'
   },
--- src/shared/components/UIElements/Map.js
+++ src/shared/components/UIElements/Map.js
@@ -3,22 +3,26 @@ import React, { useRef, useEffect } from 'react';
 import './Map.css';
 
 const Map = props => {
-	const mapRef = useRef();
+  const mapRef = useRef();
+  
+  const { center, zoom } = props;
 
-	const {center, zoom} = props;
+  useEffect(() => {
+    const map = new window.google.maps.Map(mapRef.current, {
+      center: center,
+      zoom: zoom
+    });
+  
+    new window.google.maps.Marker({ position: center, map: map });
+  }, [center, zoom]);  
 
-	useEffect(() => {		
-		const map = new window.google.maps.Map(mapRef.current, {
-			center: center,
-			zoom: zoom
-		});
-
-		new window.google.maps.Marker({position: center, map: map})
-	}, [center, zoom]);
-
-	return (
-		<div ref={mapRef} className={`map ${props.className}`} style={props.style}></div>
-	);
+  return (
+    <div
+      ref={mapRef}
+      className={`map ${props.className}`}
+      style={props.style}
+    ></div>
+  );
 };
 
 export default Map;
