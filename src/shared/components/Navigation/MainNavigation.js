9e1b114b7deb0d17cf128becf689839da559b319
zamanshahed <shaheduz.zaman@northsouth.edu>
2020-04-07 09:03:59 +0600

    initial commit 0.71: two different places added to tract the places behavior from url with userId

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -34,7 +34,7 @@ const MainNavigation = props => {
           <span />
         </button>
         <h1 className="main-navigation__title">
-          <Link to="/">YourPlaces</Link>
+          <Link to="/">CyclingPlaces</Link>
         </h1>
         <nav className="main-navigation__header-nav">
           <NavLinks />
