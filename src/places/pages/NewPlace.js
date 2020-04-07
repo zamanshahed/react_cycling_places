025c81ccb10d42753280474a660de62e7c2c6879
zamanshahed <shaheduz.zaman@northsouth.edu>
2020-04-07 17:23:31 +0600

    initial commit 0.79: add place input validation is started to work! future updates are coming!

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -6,7 +6,7 @@ import './NewPlace.css';
 const NewPlace = () => {
   return (
   	<form className="place-form">
-  		<Input element="input" type="text" label="title" />
+  		<Input element="input" type="text" label="title" validators={[]} errorText="Enter a valid title" />
   	</form>
   );
 };
