4257b229904155d8bd8c9797a898da7e4c7ec69b
zamanshahed <shaheduz.zaman@northsouth.edu>
2020-04-07 17:36:13 +0600

    initial commit 0.80: for add new place Real validation added for each key stroke!

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
 import Input from '../../shared/components/FormElements/Input';
+import { VALIDATOR_REQUIRE } from '../../shared/util/validators';
 import './NewPlace.css';
 
 const NewPlace = () => {
   return (
   	<form className="place-form">
-  		<Input element="input" type="text" label="title" validators={[]} errorText="Enter a valid title" />
+		  <Input 
+				element="input" 
+				type="text" 
+				label="title" 
+				validators={[VALIDATOR_REQUIRE()]} 
+				errorText="Enter a valid title" />
   	</form>
   );
 };
