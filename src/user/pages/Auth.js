9df5419791d0decad7276a1a6d5fc8ce6f099750
zamanshahed <shaheduz.zaman@northsouth.edu>
2020-04-10 14:07:54 +0600

    initial commit 0.89: LOGIN - SIGN UP modes are SWITCHABLE now logically !

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -1,19 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Card from '../../shared/components/UIElements/Card';
 import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
-import { VALIDATOR_EMAIL, VALIDATOR_MINLENGTH } from '../../shared/util/validators';
+import { VALIDATOR_EMAIL, VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../shared/util/validators';
 import { useForm } from '../../shared/hooks/form-hook';
 import './Auth.css';
 
 const Auth = () => {
+    const [isLoginMode, setIsLoginMode] = useState(true);
     const [formState, inputHandler] = useForm({
         email: {
             vlaue: '',
             isValid: false
         },
-        password:{
+        password: {
             vlaue: '',
             isValid: false
         }
@@ -24,11 +25,25 @@ const Auth = () => {
         console.log(formState);
     };
 
+    const switchModeHandler = () => {
+        setIsLoginMode(prevMode => !prevMode);
+    };
+
     return (
         <Card className="authentication">
             <h1>Login Required</h1>
             <hr />
             <form onSubmit={authSubmitHandler}>
+                {!isLoginMode &&
+                    <Input
+                        element="input"
+                        id="name"
+                        type="text"
+                        label="User Name"
+                        validators={[VALIDATOR_REQUIRE()]}
+                        errorText="Please enter a name."
+                        onInput={inputHandler}
+                    ></Input>}
                 <Input
                     element="input"
                     id="email"
@@ -47,8 +62,10 @@ const Auth = () => {
                     errorText="Please enter a valid password, at least 5 characters"
                     onInput={inputHandler}
                 />
-                <Button type="submit" disabled={!formState.isValid}>LOGIN</Button>
+                <Button type="submit" disabled={!formState.isValid}>{isLoginMode ? 'LOGIN' : 'SIGN UP'}</Button>
             </form>
+
+            <Button inverse onClick={switchModeHandler}>SWITCH TO {isLoginMode ? 'SIGN UP' : 'LOGIN'}</Button>
         </Card>
 
     );
