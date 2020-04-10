d1ea138112782b9680263c7c2c0b405a2e318b0a
zamanshahed <shaheduz.zaman@northsouth.edu>
2020-04-10 13:52:34 +0600

    initial commit 0.89: LOGIN PAGE UI ready!

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -1,9 +1,52 @@
 import React from 'react';
 
+import Card from '../../shared/components/UIElements/Card';
+import Input from '../../shared/components/FormElements/Input';
+import Button from '../../shared/components/FormElements/Button';
+import { VALIDATOR_EMAIL, VALIDATOR_MINLENGTH } from '../../shared/util/validators';
+import { useForm } from '../../shared/hooks/form-hook';
 import './Auth.css';
 
 const Auth = () => {
-    return <h2>Authentication Page!</h2>
+    const [formState, inputHandler] = useForm({
+        email: {
+            vlaue: '',
+            isValid: false
+        },
+        password:{
+            vlaue: '',
+            isValid: false
+        }
+    }, false);
+
+    return (
+        <Card className="authentication">
+            <h1>Login Required</h1>
+            <hr />
+            <form>
+                <Input
+                    element="input"
+                    id="email"
+                    type="email"
+                    label="E-Mail"
+                    validators={[VALIDATOR_EMAIL()]}
+                    errorText="Please enter a valid Email address.."
+                    onInput={inputHandler}
+                />
+                <Input
+                    element="input"
+                    id="password"
+                    type="password"
+                    label="Password"
+                    validators={[VALIDATOR_MINLENGTH(5)]}
+                    errorText="Please enter a valid password, at least 5 characters"
+                    onInput={inputHandler}
+                />
+                <Button type="submit" disabled={!formState.isValid}>LOGIN</Button>
+            </form>
+        </Card>
+
+    );
 };
 
 export default Auth;
